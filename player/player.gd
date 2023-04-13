e930f757cd24f16b912dea4ea4828a6039a9df23
Heartbeast <heartbeast.studios@gmail.com>
2023-04-13 14:27:14 -0600

    Character Controller + Review

--- player/player.gd
+++ player/player.gd
@@ -1,28 +1,36 @@
 extends CharacterBody2D
 
-
-const SPEED = 100.0
-const JUMP_VELOCITY = -200.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+@export var acceleration = 512
+@export var max_velocity = 64
+@export var friction = 256
+@export var gravity = 200
+@export var jump_force = 128
+@export var max_fall_velocity = 128
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	apply_gravity(delta)
+	var input_axis = Input.get_axis("ui_left", "ui_right")
+	if is_moving(input_axis):
+		apply_acceleration(delta, input_axis)
 	else:
-		velocity.x = 0
-
+		apply_friction(delta)
+	jump_check()
 	move_and_slide()
+
+func is_moving(input_axis):
+	return input_axis != 0
+
+func apply_gravity(delta):
+	if not is_on_floor():
+		velocity.y = move_toward(velocity.y, max_fall_velocity, gravity * delta)
+
+func apply_acceleration(delta, input_axis):
+	if is_moving(input_axis):
+		velocity.x = move_toward(velocity.x, input_axis * max_velocity, acceleration * delta)
+
+func apply_friction(delta):
+	velocity.x = move_toward(velocity.x, 0, friction * delta)
+
+func jump_check():
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y = -jump_force
