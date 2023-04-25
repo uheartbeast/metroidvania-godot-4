f8ec988f491b653b3ac8bf4a55768b3e3a7a4daf
Heartbeast <heartbeast.studios@gmail.com>
2023-04-25 12:18:26 -0600

    Plant Enemy + Review

---
+++ enemies/plant_enemy.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+const EnemyBulletScene = preload("res://enemies/enemy_bullet.tscn")
+
+@export var bullet_speed = 30
+@export var spread = 45
+
+@onready var stats = $Stats
+@onready var bullet_spawn_point = $BulletSpawnPoint
+@onready var fire_direction = $FireDirection
+
+func fire_bullet():
+	var bullet = Utils.instantiate_scene_on_world(EnemyBulletScene, bullet_spawn_point.global_position) as Projectile
+	var direction = global_position.direction_to(fire_direction.global_position)
+	bullet.rotation = direction.angle()
+	bullet.rotate(randf_range(-deg_to_rad(30/2), deg_to_rad(30/2)))
+	bullet.speed = bullet_speed
+	bullet.update_velocity()
+
+
+func _on_hurtbox_hurt(hitbox, damage):
+	stats.health -= damage
+
+
+func _on_stats_no_health():
+	queue_free()
+
