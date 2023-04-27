d8b22d84ae3da6e94e386a55399a382e4050f463
Heartbeast <heartbeast.studios@gmail.com>
2023-04-27 12:24:20 -0600

    Falling State for Crawling enemy + Review

--- enemies/crawling_enemy.gd
+++ enemies/crawling_enemy.gd
@@ -6,27 +6,50 @@ enum DIRECTION {LEFT = -1, RIGHT = 1}
 
 @export var crawling_direction = DIRECTION.RIGHT
 @export var max_speed = 200
+@export var spin_speed = 600
+
+var state = crawling_state
+var gravity = 100
 
 @onready var floor_cast = $FloorCast
 @onready var wall_cast = $WallCast
 @onready var stats = $Stats as Stats
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
 func _ready():
 	wall_cast.target_position.x *= crawling_direction
 
 func _physics_process(delta):
+	state.call(delta)
+
+func crawling_state(delta):
+	animated_sprite_2d.play("crawl")
 	if wall_cast.is_colliding():
 		global_position = wall_cast.get_collision_point()
 		var wall_normal = wall_cast.get_collision_normal()
 		rotation = wall_normal.rotated(deg_to_rad(90)).angle()
 	else:
 		floor_cast.rotation_degrees = -max_speed * crawling_direction * delta
+		var while_limit = 30
+		while not floor_cast.is_colliding():
+			rotation_degrees += crawling_direction
+			floor_cast.force_raycast_update()
+			while_limit -= 1
+			if while_limit <= 0:
+				state = falling_state
+				break
+			
 		if floor_cast.is_colliding():
 			global_position = floor_cast.get_collision_point()
 			var floor_normal = floor_cast.get_collision_normal()
 			rotation = floor_normal.rotated(deg_to_rad(90)).angle()
-		else:
-			rotation_degrees += crawling_direction
+
+func falling_state(delta):
+	animated_sprite_2d.play("fall")
+	rotation_degrees += crawling_direction * spin_speed * delta
+	position.y += gravity * delta
+	if floor_cast.is_colliding() or wall_cast.is_colliding():
+		state = crawling_state
 
 func _on_hurtbox_hurt(hitbox, damage):
 	stats.health -= damage
