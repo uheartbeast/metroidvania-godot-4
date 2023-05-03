ca1480ea14720db6b7bb506a1a6842fef6f61d12
Heartbeast <heartbeast.studios@gmail.com>
2023-05-03 12:07:18 -0600

    State Init + Random Fairness

--- enemies/boss_enemy.gd
+++ enemies/boss_enemy.gd
@@ -6,8 +6,12 @@ const StingerScene = preload("res://enemies/stinger.tscn")
 @export var max_speed = 800
 @export var targets : Array[NodePath]
 
-var state = rush_state
+var state = recenter_state : set = set_state
+var state_init = true : get = get_state_init
 var velocity = Vector2.ZERO
+
+var STATE_OPTIONS = [fire_state, fire_state, rush_state, rush_state]
+var state_options_left = []
 
 @onready var stats = $Stats
 @onready var stinger_pivot = $StingerPivot
@@ -15,10 +19,22 @@ var velocity = Vector2.ZERO
 @onready var firerate_timer = $FirerateTimer
 @onready var state_timer = $StateTimer
 
+func set_state(value):
+	state = value
+	state_init = true
+
+func get_state_init():
+	var was = state_init
+	state_init = false
+	return was
+
 func _process(delta):
 	state.call(delta)
 
 func rush_state(delta):
+	if state_init:
+		state_timer.start(randf_range(4.0, 6.0))
+		state_timer.timeout.connect(set_state.bind(decelertate_state), CONNECT_ONE_SHOT)
 	var player = MainInstances.player
 	if not player is Player: return
 	var direction = global_position.direction_to(player.global_position)
@@ -26,6 +42,9 @@ func rush_state(delta):
 	move(delta)
 
 func fire_state(delta):
+	if state_init:
+		state_timer.start(randf_range(4.0, 6.0))
+		state_timer.timeout.connect(set_state.bind(recenter_state), CONNECT_ONE_SHOT)
 	if firerate_timer.time_left == 0:
 		stinger_pivot.rotation_degrees += 17
 		firerate_timer.start()
@@ -35,13 +54,17 @@ func fire_state(delta):
 
 func recenter_state(delta):
 	assert(not targets.is_empty())
-	set_process(false)
-	var center_node = get_node(targets.pick_random())
-	var tween = create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
-	tween.tween_property(self, "global_position", center_node.global_position, 2.0)
-	await tween.finished
-	set_process(true)
-	state = rush_state
+	if state_init:
+		var center_node = get_node(targets.pick_random())
+		var tween = create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+		tween.tween_property(self, "global_position", center_node.global_position, 2.0)
+		await tween.finished
+		state_timer.start(0.5)
+		await state_timer.timeout
+		if state_options_left.is_empty():
+			state_options_left = STATE_OPTIONS.duplicate()
+			state_options_left.shuffle()
+		state = state_options_left.pop_back()
 
 func decelertate_state(delta):
 	velocity = velocity.move_toward(Vector2.ZERO, acceleration * delta)
@@ -57,6 +80,3 @@ func _on_hurtbox_hurt(hitbox, damage):
 
 func _on_stats_no_health():
 	queue_free()
-
-func _on_state_timer_timeout():
-	state = decelertate_state
